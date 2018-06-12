fdf5967455dc27b4d8806dc5864cb340d6ccd2bb
linpeishan <171846z@mymail.nyp.edu.sg>
2018-06-12 16:40:53 +0800

    Revert "For references"
    This reverts commit 1e2455ec13d84ea20bf3cb9a03eac2103d5a557c.

--- References/practical4_express_file_socket/server/controllers/index.js
+++
@@ -1,8 +0,0 @@
-// Show home screen
-exports.show = function(req, res) {
-	// Render home screen
-	res.render('index', {
-		title: 'ITP211 Multimedia Application',
-		callToAction: 'ITP211'
-	});
-};
