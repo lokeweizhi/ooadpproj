482c3fdc0936ff7281e053a00004c64c5f66bd7a
linpeishan <171846z@mymail.nyp.edu.sg>
2018-07-15 18:27:01 +0800

    update: able to add browse profiles through topSellers.ejs page

--- server/controllers/topSellersController.js
+++ server/controllers/topSellersController.js
@@ -4,12 +4,13 @@ var sequelize = myDatabase.sequelize;
 
 exports.list = function (req, res) {
     // [condition: must hv 10 '5/5' ratings and an average of 4.5/5]
-    sequelize.query('SELECT * FROM reviews where averageRating > 4.4 and reviewCount>9',  
+    sequelize.query('SELECT * FROM reviews where averageRating > 4.4 and reviewCount>9 ORDER BY averageRating desc',  
         { model: ReviewsModel }
     ).then(function (reviews) {
         res.render('topSellers', {
             title: "Adamire - Top Sellers",
             sellersList: reviews,
+            user: req.user,
             urlPath: req.protocol + "://" + req.get("host") + req.url
         });
     }).catch((err) => {
